6c5d53b25474f87a24fed3a361b8767d8ce27970
ayouboa30 <ayoubouladali30300@gmail.com>
2026-03-24 23:24:03 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -26,3 +26,34 @@ class MLP(nn.Module):
             x = nn.functional.softplus(layer(x))
         
         return nn.functional.softplus(self.out_layer(x))
+
+
+class ICNN(nn.Module):
+    def __init__(
+        self,
+        hidden_dim: int = 32,
+        depth: int = 3,
+        device = None,
+        dtype = None
+    ) -> None:
+        super().__init__()
+
+        self.z_layers = nn.ModuleList([
+            nn.Linear(hidden_dim, hidden_dim, device=device, dtype=dtype) 
+            for _ in range(depth - 1)
+        ])
+        
+        self.x_layers = nn.ModuleList([
+            nn.Linear(5, hidden_dim, device=device, dtype=dtype) 
+            for _ in range(depth)
+        ])
+
+        self.out_layer = nn.Linear(hidden_dim, 1, device=device, dtype=dtype)
+
+    def forward(self, x: torch.Tensor):
+        z = nn.functional.softplus(self.x_layers[0](x))
+        
+        for i in range(len(self.z_layers)):
+            z = nn.functional.softplus(self.z_layers[i](z) + self.x_layers[i+1](x))
+            
+        return self.out_layer(z)
