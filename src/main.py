d2a3fd7c1550b655a172c5ef52d9fd6b7775a30d
ayouboa30 <ayoubouladali30300@gmail.com>
2026-03-25 15:32:39 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -115,7 +115,7 @@ def main():
     #     (ConvexityLoss(0, convex=True), 1.),
     # ])
     loss_fn = CombinedLoss([
-        (RelativeMSE(epsilon=1e-4), 1.),
+        (RelativeMSE(epsilon=1e-6), 1.),
         (MonotonyLoss(1, increasing=False), 1.),
         (MonotonyLoss(0, increasing=True), 10.),
         (ConvexityLoss(1, convex=True), 1.),
